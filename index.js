935b0eda29bb3acc6e7bf55e9146cd4e5dfa7093
Jetrca92 <blaz.guzej@gmail.com>
2023-02-16 19:43:23 +0100

    Fix pen bug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Create elements for grid
 const row = document.createElement('div');
 row.setAttribute('class', 'row');
 
@@ -8,6 +9,7 @@ const container = document.querySelector('#grid-container');
 generateGrid(16);
 const btn = document.querySelector('#btn');
 
+// Function for grid size change
 function generateGrid(gridSize) {
     container.innerHTML = "";
     for (let i = 0; i < gridSize; i++) {
@@ -20,8 +22,15 @@ function generateGrid(gridSize) {
             row.appendChild(grid);
         }
     }
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => {
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('hover');
+        });
+    });
 }
 
+// When button clicked, load gridSize fnct
 btn.onclick = () => {
     let gridSize = prompt('Grid size (if input 10, grid size 10x10)?');
     gridSize = parseInt(gridSize);
@@ -30,6 +39,16 @@ btn.onclick = () => {
     }
     generateGrid(gridSize);
 }
+
+ // Add a pen-like trace to mouse pointer
+const grids = document.querySelectorAll('.grid');
+grids.forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+        grid.classList.add('hover');
+    });
+});
+
+
 
 
 
